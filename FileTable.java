e3004bf8204a76b4738802f6fb43acfb682fcc1d
Tony Thanh Le <tonytle@cssmpi1h.uwb.edu>
2022-06-05 03:52:20 -0700

    filetable complete, need testing

--- FileTable.java
+++ FileTable.java
@@ -3,6 +3,11 @@ import java.util.*;
 public class FileTable {
     private Vector table;                           // the actual entity of this file table
     private Directory dir;                          // the root directory
+    int UNUSED = 0; 
+    int USED = 1; 
+    int READ = 2; 
+    int WRITE = 3; 
+    int TODELETE = 4; 
     public FileTable( Directory directory ) {       // constructor
         table = new Vector( );                      // instantiate a file (structure) table
         dir = directory;                            // receive a reference to the Director
@@ -17,19 +22,26 @@ public class FileTable {
             iNumber = (filename.equals("/") ? 0 : dir.namei (filename)); 
             if (iNumber >= 0) {
                 inode = new Inode(iNumber); 
-                if (mode.equals("r")) { 
-                    if (inode.flag == 2) {
+                if (mode.compareTo("r") == 0) {  // if mode == read 
+                    if (inode.flag == READ) {
                         break; 
-                    } else if (inode.flag == 3) {
+                    } else if (inode.flag == WRITE) {
                         try { wait(); } catch(InterruptedException e) {} 
-                    } else if (inode.flag == 4) { 
+                    } else if (inode.flag == TODELETE) { 
                         iNumber = -1; 
                         return null; 
                     }
-                } else if (mode.equals("w")) {
-                    // not done yet. 
-
+                } else {    // if mode is w, w+, a
+                    if (inode.flag == USED || inode.flag == UNUSED) { 
+                        inode.flag = (short)WRITE; 
+                        break; 
+                    } else { 
+                        try { wait(); } catch(InterruptedException e) {} 
+                    }
                 }
+            } else { // if inode is less than 0, file doesnt exist 
+                iNumber = dir.ialloc(filename); // allocate inum
+                inode = new Inode();            // make node for file 
             }
 
         }
@@ -46,6 +58,14 @@ public class FileTable {
     }
 
     public synchronized boolean ffree( FileTableEntry e ) {
+        if (table.contains(e) == true) {
+            e.inode.flag = (short)UNUSED; 
+            e.inode.toDisk(e.iNumber);
+            table.remove(e); 
+            notifyAll();
+            return true;
+        }
+        return false; 
     // receive a file table entry reference
     // save the corresponding inode to the disk
     // free this file table entry.
