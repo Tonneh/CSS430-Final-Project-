46f1d4b1258fc1885fb4adfa6ddaab701e650c86
Tony Thanh Le <tonytle@cssmpi1h.uwb.edu>
2022-06-08 23:36:25 -0700

    comments done for FileTable.java

--- FileTable.java
+++ FileTable.java
@@ -15,71 +15,62 @@ public class FileTable {
 
     // major public methods
     public synchronized FileTableEntry falloc( String filename, String mode ) {
-
         short iNumber = -1; 
         Inode inode = null; 
         while (true) {
             iNumber = (filename.equals("/") ? 0 : dir.namei(filename)); 
             if (iNumber >= 0) {
                 inode = new Inode(iNumber); 
-                if (mode.compareTo("r") == 0) {  // if mode == read 
-                    if (inode.flag == UNUSED || inode.flag == USED) {
-                        inode.flag = (short)READ; 
+                if (mode.compareTo("r") == 0) {                         // if mode == read 
+                    if (inode.flag == UNUSED || inode.flag == USED) {   // check if used to unused
+                        inode.flag = (short)READ;                       // if so then set to read and break 
                         break; 
                     }
-                    else if (inode.flag <= READ) {
+                    else if (inode.flag <= READ) {                      // if flag is read then just break 
                         break; 
-                    } else if (inode.flag == WRITE) {
+                    } else if (inode.flag == WRITE) {                   // if flag is write we need to wait 
                         try { wait(); } catch(InterruptedException e) {} 
-                    } else if (inode.flag == TODELETE) { 
+                    } else if (inode.flag == TODELETE) {                // if flag is todelete then set inum to -1 and return null 
                         iNumber = -1; 
                         return null; 
                     }
-                } else if (mode.compareTo("w") == 0|| mode.compareTo("w+") == 0 || mode.compareTo("a") == 0) {    // if mode is w, w+, a
-                    if (inode.flag == USED || inode.flag == UNUSED) { 
+                } else if (mode.compareTo("w") == 0||                   // if mode is w, w+, a
+                mode.compareTo("w+") == 0 || mode.compareTo("a") == 0) {
+                    if (inode.flag == USED || inode.flag == UNUSED) {   // if flag is unused or used set to write then break; 
                         inode.flag = (short)WRITE; 
                         break; 
-                    } else { 
+                    } else {                                            // else we just wait 
                         try { wait(); } catch(InterruptedException e) {} 
                     }
                 }
-            } else if (! (mode.compareTo("r") == 0)) { // if inode is less than 0, file doesnt exist 
-                iNumber = dir.ialloc(filename); // allocate inum
-                inode = new Inode();            // make node for file 
-                inode.flag = (short)WRITE; 
+            } else if (! (mode.compareTo("r") == 0)) {                  // if inode is less than 0, file doesnt exist 
+                iNumber = dir.ialloc(filename);                         // allocate inum
+                inode = new Inode();                                    // make node for file 
+                inode.flag = (short)WRITE;                              // set flag to write 
                 break; 
             } else {
                 return null; 
             }
         }
-        inode.count++; 
-        inode.toDisk(iNumber); 
-        FileTableEntry e = new FileTableEntry(inode, iNumber, mode); 
-        table.addElement(e); 
-        return e; 
-    // allocate a new file (structure) table entry for this file name
-    // allocate/retrieve and register the corresponding inode using dir
-    // increment this inode's count
-    // immediately write back this inode to the disk
-    // return a reference to this file (structure) table entry
+        inode.count++;                                                  // increment count
+        inode.toDisk(iNumber);                                          // add to disk 
+        FileTableEntry e = new FileTableEntry(inode, iNumber, mode);    // make new ftent
+        table.addElement(e);                                            // add to table 
+        return e;                                                       // return ftent
     }
 
     public synchronized boolean ffree( FileTableEntry e ) {
-        if (table.contains(e) == true) {
-            e.inode.flag = (short)UNUSED; 
-            e.inode.toDisk(e.iNumber);
-            table.remove(e); 
-            notifyAll();
+        if (table.contains(e) == true) {                                // check if table contains ftent
+            e.inode.flag = (short)UNUSED;                               // if so set to unused 
+            e.inode.toDisk(e.iNumber);                                  // add to disk the inum
+            table.remove(e);                                            // remove from table
+            notifyAll();                                                // notify all to wake up all threads waiting on this
             return true;
         }
         return false; 
-    // receive a file table entry reference
-    // save the corresponding inode to the disk
-    // free this file table entry.
-    // return true if this file table entry found in my table
     }
 
     public synchronized boolean fempty( ) {
         return table.isEmpty( ); // return if table is empty
-    } // should be called before starting a format
+    }
 }
